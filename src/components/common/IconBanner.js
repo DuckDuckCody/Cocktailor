cadb34e4e930c78e95673fd1d2b1cf4f4f20cd92
Cody Hennighan <cody.hennighan@gmail.com>
2018-04-17 22:57:58 +0800

    update CNAME

---
+++ src/components/common/IconBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const IconBanner = (props) => {
+  const style = {
+
+  }
+
+  return (
+    <div className="icon-banner">
+      <span> Suggest Me Cocktails </span>
+    </div>
+  );
+}
+
+export default IconBanner;
